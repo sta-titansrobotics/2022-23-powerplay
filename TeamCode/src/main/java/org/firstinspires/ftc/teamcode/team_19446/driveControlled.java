94528300c0a732975662b9920eb97b599b031758
shan <shan2005an@gmail.com>
2022-09-27 15:21:11 -0400

    very preliminary drivetrain using info we have

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/team_19446/driveControlled.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/team_19446/driveControlled.java
@@ -24,24 +24,12 @@ public class driveControlled extends LinearOpMode {
         motorFR.setDirection(DcMotorSimple.Direction.REVERSE);
         motorBR.setDirection(DcMotorSimple.Direction.REVERSE);
 
-        //Intake
-        DcMotor Intake = hardwareMap.get(DcMotor.class, "intake");
-
-        Intake.setDirection(DcMotorSimple.Direction.REVERSE);
-
-        //Arm
+     
+        //Cascading Arm
         DcMotor Arm = hardwareMap.get(DcMotor.class, "arm");
         Arm.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
-        //Carousel
-        DcMotor Carousel = hardwareMap.get(DcMotor.class, "carousel");
-
-        Carousel.setDirection(DcMotorSimple.Direction.REVERSE);
-
-        //Turret
-        DcMotor Turret = hardwareMap.get(DcMotor.class, "turret");
-        Turret.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-
+        
 
         waitForStart();
 
@@ -99,44 +87,20 @@ public class driveControlled extends LinearOpMode {
                 motorBR.setPower(1);
             }
 
-            //Intake
-            double intakePower = gamepad2.left_stick_y * 0.80;
-
-            Intake.setPower(intakePower);
+            
 
             //Arm
             double armPower = gamepad2.right_stick_y;
 
             Arm.setPower(armPower);
-
-            //Carousel
-
-            if (gamepad2.a) {
-                Carousel.setPower(0.85);
-
-            }
-            else {
-                Carousel.setPower(0);
-
-            }
-
-            //Turret
-            double turretPowerLeft = gamepad2.left_trigger;
-            Turret.setPower(turretPowerLeft);
-
-            double turretPowerRight = gamepad2.right_trigger;
-            Turret.setPower(-turretPowerRight);
-
+            
 
 
             telemetry.addData("LF Power:", motorFL.getPower());
             telemetry.addData("LB Power:", motorBL.getPower());
             telemetry.addData("RF Power:", motorFR.getPower());
             telemetry.addData("RB Power:", motorBR.getPower());
-            telemetry.addData("Intake Power: ", Intake.getPower());
             telemetry.addData("Arm Power:", Arm.getPower());
-            telemetry.addData("Carousel Power:", Carousel.getPower());
-            telemetry.addData("Turret Power:", Turret.getPower());
             telemetry.addData("Arm Encoder Position: ", Arm.getCurrentPosition());
             telemetry.update();
 

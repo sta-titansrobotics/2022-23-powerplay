3b164f5d1b692cd9d86b08bdd7872887badd66ca
Andrew <andrew.chan23@ycdsbk12.ca>
2022-09-29 15:02:13 -0400

    Fix drivetrain

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/team_19446/driveControlled.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/team_19446/driveControlled.java
@@ -21,27 +21,8 @@ public class driveControlled extends LinearOpMode {
         DcMotor motorBR = hardwareMap.get(DcMotor.class, "motorBackRight");
 
         //Reverse right side motors
-        motorFR.setDirection(DcMotorSimple.Direction.REVERSE);
-        motorBR.setDirection(DcMotorSimple.Direction.REVERSE);
-
-        //Intake
-        DcMotor Intake = hardwareMap.get(DcMotor.class, "intake");
-
-        Intake.setDirection(DcMotorSimple.Direction.REVERSE);
-
-        //Arm
-        DcMotor Arm = hardwareMap.get(DcMotor.class, "arm");
-        Arm.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-
-        //Carousel
-        DcMotor Carousel = hardwareMap.get(DcMotor.class, "carousel");
-
-        Carousel.setDirection(DcMotorSimple.Direction.REVERSE);
-
-        //Turret
-        DcMotor Turret = hardwareMap.get(DcMotor.class, "turret");
-        Turret.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-
+        motorFL.setDirection(DcMotorSimple.Direction.REVERSE);
+        motorBL.setDirection(DcMotorSimple.Direction.REVERSE);
 
         waitForStart();
 
@@ -71,73 +52,10 @@ public class driveControlled extends LinearOpMode {
             motorFR.setPower(frontRightPower);
             motorBR.setPower(backRightPower);
 
-            if (gamepad1.dpad_up) {
-                motorFL.setPower(1);
-                motorBL.setPower(1);
-                motorFR.setPower(1);
-                motorBR.setPower(1);
-            }
-
-            if (gamepad1.dpad_down) {
-                motorFL.setPower(-1);
-                motorBL.setPower(-1);
-                motorFR.setPower(-1);
-                motorBR.setPower(-1);
-            }
-
-            if (gamepad1.dpad_left) {
-                motorFL.setPower(-1);
-                motorBL.setPower(1);
-                motorFR.setPower(1);
-                motorBR.setPower(-1);
-            }
-
-            if (gamepad1.dpad_right) {
-                motorFL.setPower(1);
-                motorBL.setPower(-1);
-                motorFR.setPower(-1);
-                motorBR.setPower(1);
-            }
-
-            //Intake
-            double intakePower = gamepad2.left_stick_y * 0.80;
-
-            Intake.setPower(intakePower);
-
-            //Arm
-            double armPower = gamepad2.right_stick_y;
-
-            Arm.setPower(armPower);
-
-            //Carousel
-
-            if (gamepad2.a) {
-                Carousel.setPower(0.85);
-
-            }
-            else {
-                Carousel.setPower(0);
-
-            }
-
-            //Turret
-            double turretPowerLeft = gamepad2.left_trigger;
-            Turret.setPower(turretPowerLeft);
-
-            double turretPowerRight = gamepad2.right_trigger;
-            Turret.setPower(-turretPowerRight);
-
-
-
             telemetry.addData("LF Power:", motorFL.getPower());
             telemetry.addData("LB Power:", motorBL.getPower());
             telemetry.addData("RF Power:", motorFR.getPower());
             telemetry.addData("RB Power:", motorBR.getPower());
-            telemetry.addData("Intake Power: ", Intake.getPower());
-            telemetry.addData("Arm Power:", Arm.getPower());
-            telemetry.addData("Carousel Power:", Carousel.getPower());
-            telemetry.addData("Turret Power:", Turret.getPower());
-            telemetry.addData("Arm Encoder Position: ", Arm.getCurrentPosition());
             telemetry.update();
 
 

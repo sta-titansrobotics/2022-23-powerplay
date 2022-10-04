f125de10b07c37ba0eaf0a517dac4b3df170c236
Andrew Ho <Andrew.ho8999@gmail.com>
2022-10-04 15:49:30 -0400

    Android Studio Test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/team_19446/iosfiashfosd.java
+++
@@ -1,5 +0,0 @@
-package org.firstinspires.ftc.teamcode.team_19446;
-
-public class iosfiashfosd {
-    //snsdufhisudfhsdfsdsdjfosdjf
-}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/team_19446/thingy.java
+++
@@ -1,7 +0,0 @@
-package org.firstinspires.ftc.teamcode.team_19446;
-
-public class thingy {
-
-
-    //shmexy
-}

76a4295e98ea699f76c3f92b852f589c6a0faf13
liana <austin.lian25@ycdsbk12.ca>
2022-10-25 15:06:40 -0400

    Android studio commit

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/team_19447/Intake.java
@@ -0,0 +1,37 @@
+package org.firstinspires.ftc.teamcode.team_19447;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.Range;
+
+@TeleOp
+public class IntakeBracket extends LinearOpMode {
+    DcMotor rollerFlipper = null;
+
+    @Override
+    public void runOpMode() {
+        //initialize the motor
+        rollerFlipper = hardwareMap.dcMotor.get("flipperMotor");
+        //set motor direction
+        rollerFlipper.setDirection(DcMotor.Direction.FORWARD);
+        //use encoder or not?
+        rollerFlipper.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        //set power
+        double flipperMotorPower;
+
+        //wait for start button to be pushed
+        waitForStart();
+
+        while (opModeIsActive()) {
+
+            flipperMotorPower = gamepad1.touchpad_finger_1_y;
+            flipperMotorPower = Range.clip(flipperMotorPower, -1, 1);
+            rollerFlipper.setPower(flipperMotorPower);
+
+        }
+
+
+    }
+
+}

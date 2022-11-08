3c20b30b66451208de89a3a8da0d6237cb9f450b
LianAustin <austin.lian25@ycdsbk12.ca>
2022-11-08 16:06:25 -0500

    Android studio commit

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/team_19447/Auto447.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/team_19447/Auto447.java
@@ -4,6 +4,7 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
@@ -36,124 +37,162 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 @Autonomous(name="Auto447", group="Robot")
 //@Disabled
 public class Auto447 extends LinearOpMode {
+    //Set motor variables
+    private DcMotor motorFL;
+    private DcMotor motorBL;
+    private DcMotor motorFR;
+    private DcMotor motorBR;
 
-    /* Declare OpMode members. */
-    private DcMotor         leftDrive   = null;
-    private DcMotor         rightDrive  = null;
+    //Initializing encoder positions
+    private int leftPos1;
+    private int leftPos2;
+    private int rightPos1;
+    private int rightPos2;
 
-    private ElapsedTime     runtime = new ElapsedTime();
-
-    // Calculate the COUNTS_PER_INCH for your specific drive train.
-    // Go to your motor vendor website to determine your motor's COUNTS_PER_MOTOR_REV
-    // For external drive gearing, set DRIVE_GEAR_REDUCTION as needed.
-    // For example, use a value of 2.0 for a 12-tooth spur gear driving a 24-tooth spur gear.
-    // This is gearing DOWN for less speed and more torque.
-    // For gearing UP, use a gear ratio less than 1.0. Note this will affect the direction of wheel rotation.
-    static final double     COUNTS_PER_MOTOR_REV    = 1440 ;    // eg: TETRIX Motor Encoder
-    static final double     DRIVE_GEAR_REDUCTION    = 1.0 ;     // No External Gearing.
-    static final double     WHEEL_DIAMETER_INCHES   = 4.0 ;     // For figuring circumference
-    static final double     COUNTS_PER_INCH         = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) /
-            (WHEEL_DIAMETER_INCHES * 3.1415);
-    static final double     DRIVE_SPEED             = 0.6;
-    static final double     TURN_SPEED              = 0.5;
-
-    @Override
     public void runOpMode() {
 
-        // Initialize the drive system variables.
-        leftDrive  = hardwareMap.get(DcMotor.class, "left_drive");
-        rightDrive = hardwareMap.get(DcMotor.class, "right_drive");
+        //Initialize motors
+        motorFL = hardwareMap.get(DcMotor.class, "motorFrontLeft");
+        motorBL = hardwareMap.get(DcMotor.class, "motorBackLeft");
+        motorFR = hardwareMap.get(DcMotor.class, "motorFrontRight");
+        motorBR = hardwareMap.get(DcMotor.class, "motorBackRight");
 
-        // To drive forward, most robots need the motor on one side to be reversed, because the axles point in opposite directions.
-        // When run, this OpMode should start both motors driving forward. So adjust these two lines based on your first test drive.
-        // Note: The settings here assume direct drive on left and right wheels.  Gear Reduction or 90 Deg drives may require direction flips
-        leftDrive.setDirection(DcMotor.Direction.REVERSE);
-        rightDrive.setDirection(DcMotor.Direction.FORWARD);
+        //set mode to stop and reset encoders -- resets encoders to the 0 position
+        motorFL.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        motorBL.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        motorFR.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        motorBR.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
-        leftDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        rightDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        //Reverse left side motors
+        motorFL.setDirection(DcMotorSimple.Direction.REVERSE);
+        motorBL.setDirection(DcMotorSimple.Direction.REVERSE);
 
-        leftDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        rightDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        //Initialize the positions to zero, since the motor has not moved yet
+        leftPos1 = 0;
+        leftPos2 = 0;
+        rightPos1 = 0;
+        rightPos2 = 0;
 
-        // Send telemetry message to indicate successful Encoder reset
-        telemetry.addData("Starting at",  "%7d :%7d",
-                leftDrive.getCurrentPosition(),
-                rightDrive.getCurrentPosition());
-        telemetry.update();
+        motorFL.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        motorBL.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        motorFR.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        motorBR.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
-        // Wait for the game to start (driver presses PLAY)
+        //any code after this command will not be executed until the match has started
         waitForStart();
 
-        // Step through each leg of the path,
-        // Note: Reverse movement is obtained by setting a negative distance (not speed)
-        encoderDrive(DRIVE_SPEED,  48,  48, 5.0);  // S1: Forward 47 Inches with 5 Sec timeout
-        encoderDrive(TURN_SPEED,   12, -12, 4.0);  // S2: Turn Right 12 Inches with 4 Sec timeout
-        encoderDrive(DRIVE_SPEED, -24, -24, 4.0);  // S3: Reverse 24 Inches with 4 Sec timeout
+        //can now set drive distance because of the function below; now we just need to input the distance
+        //can also control the direction using the mecanum drivetrain directions here: https://gm0.org/en/latest/docs/software/tutorials/mecanum-drive.html
 
-        telemetry.addData("Path", "Complete");
-        telemetry.update();
-        sleep(1000);  // pause to display final telemetry message.
+        //ex: this command will get the robot to travel forward (all target values are positive) for 10cm at a speed of 2
+        drive(10, 10, 10, 10, 1);
+
+        //ex: this command will get the robot to strafe left for 9cm at a speed of 1
+        drive(-9, 9, 9, -9, 1);
+
+        motorFL.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        motorBL.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        motorFR.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        motorBR.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        while (opModeIsActive()) {
+            telemetry.addData("motorFL Encoder Position: ", motorFL.getCurrentPosition());
+            telemetry.addData("motorBL Encoder Position: ", motorBL.getCurrentPosition());
+            telemetry.addData("motorFR Encoder Position: ", motorFR.getCurrentPosition());
+            telemetry.addData("motorBR Encoder Position: ", motorBR.getCurrentPosition());
+            telemetry.update();
+        }
     }
 
-    /*
-     *  Method to perform a relative move, based on encoder counts.
-     *  Encoders are not reset as the move is based on the current position.
-     *  Move will stop if any of three conditions occur:
-     *  1) Move gets to the desired position
-     *  2) Move runs out of time
-     *  3) Driver stops the opmode running.
-     */
-    public void encoderDrive(double speed,
-                             double leftInches, double rightInches,
-                             double timeoutS) {
-        int newLeftTarget;
-        int newRightTarget;
+    //will use a function that will take the distance and speed of the motors based on the rotation
+    //void because no return value
+    public void drive(int leftTarget1, int leftTarget2, int rightTarget1, int rightTarget2, double speed) {
 
-        // Ensure that the opmode is still active
-        if (opModeIsActive()) {
+        double forwardTicks = 52.3;
+        double strafeTicks = 54.05;
 
-            // Determine new target position, and pass to motor controller
-            newLeftTarget = leftDrive.getCurrentPosition() + (int)(leftInches * COUNTS_PER_INCH);
-            newRightTarget = rightDrive.getCurrentPosition() + (int)(rightInches * COUNTS_PER_INCH);
-            leftDrive.setTargetPosition(newLeftTarget);
-            rightDrive.setTargetPosition(newRightTarget);
+        int ticks1 = (int) forwardTicks;
+        int ticks2 = (int) strafeTicks;
 
-            // Turn On RUN_TO_POSITION
-            leftDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-            rightDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        leftPos1 += leftTarget1; //By adding the "+=", it makes it equivalent to leftPos1 = leftPos1 + leftTarget1, therefore it will allow adding values to the position based on what the target is.
+        leftPos2 += leftTarget2; //This will therefore change where the motor needs to be by the specific inputted amount
+        rightPos1 += rightTarget1;
+        rightPos2 += rightTarget2;
 
-            // reset the timeout time and start motion.
-            runtime.reset();
-            leftDrive.setPower(Math.abs(speed));
-            rightDrive.setPower(Math.abs(speed));
+        // Using setTargetPosition and RUN_TO_POSITION, it forces motors to continue running until the encoders reach the specified target position
+        // We are multiplying by forward/strafe ticks in relation to forward/strafe based on what the robot is doing.
+        if ((leftPos1 >= 0) && (leftPos2 >= 0) && (rightPos1 >= 0) && (rightPos2 >= 0)) { //Forward
+            motorFL.setTargetPosition((leftPos1) * ticks1);
+            motorBL.setTargetPosition((leftPos2) * ticks1);
+            motorFR.setTargetPosition((rightPos1) * ticks1);
+            motorBR.setTargetPosition((rightPos2) * ticks1);
 
-            // keep looping while we are still active, and there is time left, and both motors are running.
-            // Note: We use (isBusy() && isBusy()) in the loop test, which means that when EITHER motor hits
-            // its target position, the motion will stop.  This is "safer" in the event that the robot will
-            // always end the motion as soon as possible.
-            // However, if you require that BOTH motors have finished their moves before the robot continues
-            // onto the next step, use (isBusy() || isBusy()) in the loop test.
-            while (opModeIsActive() &&
-                    (runtime.seconds() < timeoutS) &&
-                    (leftDrive.isBusy() && rightDrive.isBusy())) {
+        } else if ((leftPos1 < 0) && (leftPos2 < 0) && (rightPos1 < 0) && (rightPos2 < 0)) { //Backwards
+            motorFL.setTargetPosition((leftPos1) * ticks1);
+            motorBL.setTargetPosition((leftPos2) * ticks1);
+            motorFR.setTargetPosition((rightPos1) * ticks1);
+            motorBR.setTargetPosition((rightPos2) * ticks1);
 
-                // Display it for the driver.
-                telemetry.addData("Running to",  " %7d :%7d", newLeftTarget,  newRightTarget);
-                telemetry.addData("Currently at",  " at %7d :%7d",
-                        leftDrive.getCurrentPosition(), rightDrive.getCurrentPosition());
-                telemetry.update();
-            }
+        } else if (((leftPos1 < 0) && (rightPos2 < 0) && (leftPos2 >= 0) && (rightPos1 >= 0)) || (((leftPos2 < 0) && (rightPos1 < 0)) && (leftPos1 >= 0) && (rightPos2 >= 0))) {
+            // Strafing left or right
+            motorFL.setTargetPosition((leftPos1) * ticks2);
+            motorBL.setTargetPosition((leftPos2) * ticks2);
+            motorFR.setTargetPosition((rightPos1) * ticks2);
+            motorBR.setTargetPosition((rightPos2) * ticks2);
 
-            // Stop all motion;
-            leftDrive.setPower(0);
-            rightDrive.setPower(0);
+        } else if (((leftPos2 > 0) && (rightPos1 > 0)) || ((leftPos1 > 0) && (rightPos2 > 0))) {
+            //Strafing diagonally forward right or left, respectively.
+            motorFL.setTargetPosition((leftPos1) * ticks2);
+            motorBL.setTargetPosition((leftPos2) * ticks2);
+            motorFR.setTargetPosition((rightPos1) * ticks2);
+            motorBR.setTargetPosition((rightPos2) * ticks2);
 
-            // Turn off RUN_TO_POSITION
-            leftDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-            rightDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        } else if (((leftPos2 < 0) && (rightPos1 < 0)) || ((leftPos1 < 0) && (rightPos2 < 0))) {
+            //Strafing diagonally backward right or left, respectively.
+            motorFL.setTargetPosition((leftPos1) * ticks2);
+            motorBL.setTargetPosition((leftPos2) * ticks2);
+            motorFR.setTargetPosition((rightPos1) * ticks2);
+            motorBR.setTargetPosition((rightPos2) * ticks2);
 
-            sleep(250);   // optional pause after each move.
+        } else if (((leftPos1 < 0) && (rightPos2 > 0) && (leftPos2 < 0) && (rightPos1 > 0)) || (((leftPos2 > 0) && (rightPos1 < 0)) && (leftPos1 > 0) && (rightPos2 < 0))) {
+            //Turning left or right, respectively.
+            motorFL.setTargetPosition((leftPos1) * ticks2);
+            motorBL.setTargetPosition((leftPos2) * ticks2);
+            motorFR.setTargetPosition((rightPos1) * ticks2);
+            motorBR.setTargetPosition((rightPos2) * ticks2);
+
         }
+
+        // The code gets stuck in between the Run to Position and the speed.
+        motorFL.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        motorBL.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        motorFR.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        motorBR.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        //Encoders do not change speed automatically. Need to adjust speed ourselves
+        motorFL.setPower(speed);
+        motorBL.setPower(speed);
+        motorFR.setPower(speed);
+        motorBR.setPower(speed);
+
+        //while loop to stall/delay the next command
+        while (motorFL.isBusy() && motorBL.isBusy() && motorFR.isBusy() && motorBR.isBusy()) {
+            telemetry.addData("motorFL Encoder Position: ", motorFL.getCurrentPosition());
+            telemetry.addData("motorBL Encoder Position: ", motorBL.getCurrentPosition());
+            telemetry.addData("motorFR Encoder Position: ", motorFR.getCurrentPosition());
+            telemetry.addData("motorBR Encoder Position: ", motorBR.getCurrentPosition());
+            telemetry.update();
+        }
+
+        //Stop driving so that it can perform the next command.
+        motorFL.setPower(0);
+        motorBL.setPower(0);
+        motorFR.setPower(0);
+        motorBR.setPower(0);
+        motorFL.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        motorBL.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        motorFR.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        motorBR.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
     }
 }

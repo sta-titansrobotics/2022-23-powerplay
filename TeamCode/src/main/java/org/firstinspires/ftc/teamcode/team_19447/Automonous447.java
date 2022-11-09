e05ce37e32f7c6f287322e453c35742196fb3357
LianAustin <austin.lian25@ycdsbk12.ca>
2022-11-08 23:21:34 -0500

    + Added back code accidently deleted in Automonous447.java + Fixed ConePicker.java (Kind of)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/team_19447/Automonous447.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/team_19447/Automonous447.java
@@ -8,7 +8,7 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
-public class Automonous447{
+public class Automonous447 extends LinearOpMode {
 
     DcMotor leftF, leftR, rightR, rightF;
     public static double forwardTicks;
@@ -187,6 +187,27 @@ public class Automonous447{
         rightR.setMode(mode);
     }
     //This must be placed AFTER all of the creations of methods or else everything breaks
+    public void runOpMode(){
+        /*
+        * Example Autonomous
+        * Moves forward, goes left and right, goes back to original position
+        */
+        forward(50);
+        leftStrafe(50);
+        rightStrafe(100);
+        leftStrafe(50);
+        backward(50);
+
+        //Logs encoder positions to telemetry
+        while (opModeIsActive()) {
+
+            telemetry.addData("encoder-front-left: ", leftF.getCurrentPosition());
+            telemetry.addData("encoder-rear-left: ", leftR.getCurrentPosition());
+            telemetry.addData("encoder-front-right: ", rightF.getCurrentPosition());
+            telemetry.addData("encoder-rear-right: ", rightR.getCurrentPosition());
+            telemetry.update();
+        }
+    }
 
 }
 
